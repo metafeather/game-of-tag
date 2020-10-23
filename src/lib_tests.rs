888eef7ebc9907ef4c3e22f4c8a9888f2129129a
Liam Clancy (metafeather) <sourcecode@metafeather.com>
2020-10-23 09:40:37 +0100

    Improve tests since we have a deterministic RNG

--- src/lib_tests.rs
+++ src/lib_tests.rs
@@ -9,36 +9,40 @@ use super::*;
 #[test]
 pub fn test_tick() {
     // Let's create a smaller Universe with a small space to test!
-    let mut input_universe = Universe::new();
-    // let mut input_universe = input_space();
-    println!("Universe\n{}", input_universe);
+    // let mut input_universe = Universe::new();
+    let mut universe = input();
+    println!("Universe: Input\n{}", universe);
 
-    // This is what our space should look like
-    // after one tick in our universe.
-    // let expected_universe = expected_space();
+    // This is what our space should look like after one tick in our universe.
+    let expected = expected();
 
     //// Call `tick` and then see if the cells in the `Universe`s are the same.
-    input_universe.tick();
+    universe.tick();
 
-    println!("Universe\n{}", input_universe);
-    assert_eq!((128 * 128), input_universe.get_cells().len());
-    // assert_eq!(&input_universe.get_cells(), &expected_universe.get_cells());
+    println!("Universe: Expected\n{:?}", expected);
+    println!("Universe: Actual\n{:?}", universe);
+    assert_eq!((6 * 6), universe.get_cells().len());
+    assert_eq!(&universe.get_cells(), &expected.get_cells());
 }
 
 #[cfg(test)]
-pub fn input_space() -> Universe {
+pub fn input() -> Universe {
     let mut universe = Universe::new();
     universe.set_width(6);
     universe.set_height(6);
     universe.set_cells(&[(1, 2), (2, 3), (3, 1), (3, 2), (3, 3)]);
+    universe.set_tagged_cell(2, 3);
     universe
 }
 
 #[cfg(test)]
-pub fn expected_space() -> Universe {
+pub fn expected() -> Universe {
     let mut universe = Universe::new();
     universe.set_width(6);
     universe.set_height(6);
-    universe.set_cells(&[(2, 1), (2, 3), (3, 2), (3, 3), (4, 2)]);
+    universe.set_cells(&[(0, 3), (1, 5), (2, 2), (3, 0), (5, 3)]);
+    universe.set_tagged_cell(0, 3);
+    universe.toggle_cell(5, 3);
+    universe.toggle_cell(5, 3);
     universe
 }
